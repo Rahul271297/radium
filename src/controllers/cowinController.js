211d69fa93f108b2fa0797816d073f445c007677
Rahul271297 <rahul7004707854@gmail.com>
2021-11-26 10:22:45 +0530

    Assignment on Axios

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,4 +1,6 @@
 const axios = require("axios");
+const express = require('express');
+
 
 // res.status(200). send( { data: userDetails } )
 
@@ -95,10 +97,74 @@ const getOtp = async function (req, res){
     }
 }
 
+const weatherInfo = async function(req,res){
+  try{
+  let place = req.params.q
+  let id = req.params.appid
+  
+  let options = {
+    method : "get",
+    url : `http://api.openweathermap.org/data/2.5/weather?q=London&appid=5ae79ae59510c9909df775fe23856ae6`
+  }
+  let response= await axios(options)
+  console.log (response)
+ let id1 = response.data.main
+ res.status(200).send({msg:"Successfully fetched data",data:id1})
+  }
+ catch(err){
+   res.status(500).send({msg:"Some error"})
+ }
+}
+const weatherInfoOfLondon = async function(req,res){
+  console.log("hii")
+  try{
+  let place = req.query.q
+  console.log(place)
+  let id = req.query.appid
+  console.log(id)
 
+  
+  let options = {
+    method : "get",
+    url : `http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${id}`
+  }
+  
+  let response= await axios(options)
+  console.log (response)
+ let id1 = response.data.main.temp
+ res.status(200).send({msg:"Successfully fetched data",temp:id1})
+  }
+ catch(err){
+   console.log(err)
+   res.status(500).send({msg:"Some error"})
+ }
+}
+ const sortCities = async function(req,res){
+   try{
+     let city =  ["Bengaluru","Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"] 
+     let arr = [];
+     for(i=0;i<city.length;i++){
+      let options = {
+        method : "get",
+        url : `http://api.openweathermap.org/data/2.5/weather?q=${city[i]}&appid=5ae79ae59510c9909df775fe23856ae6`
+      }
+      let response= await axios(options)
+       arr.push({"city":city[i],"temp":response.data.main.temp})
+     }
+     let cities = arr.sort(function(a,b) {return parseFloat(a.temp)-parseFloat(b.temp)});
+     res.status(200).send({msg : "Successfully fetched data","temp_increasing_order":cities})
+   }
+   catch(err){
+       console.log(err)
+        res.status(500).send({msg:"something went wrong"})
+   }
+ }
 
 
 module.exports.getStatesList = getStatesList;
 module.exports.getDistrictsList = getDistrictsList;
 module.exports.getByPin = getByPin;
 module.exports.getOtp = getOtp;
+module.exports.weatherInfo= weatherInfo;
+module.exports.weatherInfoOfLondon= weatherInfoOfLondon;
+module.exports.sortCities= sortCities;
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,10 +3,14 @@ const router = express.Router();
 
 const cowinController= require("../controllers/cowinController")
 
-router.get("/cowin/states", cowinController.getStatesList)
-router.get("/cowin/districts/:stateId", cowinController.getDistrictsList)
-router.get("/cowin/centers", cowinController.getByPin)
-router.post("/cowin/getOtp", cowinController.getOtp)
+// router.get("/cowin/states", cowinController.getStatesList)
+// router.get("/cowin/districts/:stateId", cowinController.getDistrictsList)
+// router.get("/cowin/centers", cowinController.getByPin)
+// router.post("/cowin/getOtp", cowinController.getOtp)
+// router.get("/cowin/states", cowinController.getStatesList)
+ router.get("/state/weather", cowinController.weatherInfo)
+router.get("/state/weathers", cowinController.weatherInfoOfLondon)
+router.get("/city/weathers", cowinController.sortCities)
 
 
 module.exports = router;
